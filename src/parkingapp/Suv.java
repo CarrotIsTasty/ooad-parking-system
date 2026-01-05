381f9acd89098e950d8d438ffd0bb9aaefc8771d
CarrotIsTasty <138109195+CarrotIsTasty@users.noreply.github.com>
2026-01-05 12:46:42 +0800

    Remove isHandicapped

--- src/parkingapp/Suv.java
+++ src/parkingapp/Suv.java
@@ -1,7 +1,7 @@
 package parkingapp;
 
 public class Suv extends Vehicle{
-    public Suv(String platenumber, boolean isHandicapped, boolean isVip){
-        super(platenumber, isHandicapped, isVip, VehicleType.SUV);
+    public Suv(String platenumber, boolean isVip){
+        super(platenumber, isVip, VehicleType.SUV);
     }
 }
