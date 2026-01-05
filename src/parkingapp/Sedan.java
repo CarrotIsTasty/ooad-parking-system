0774180d6d382161f426b181d09a029135479235
CarrotIsTasty <138109195+CarrotIsTasty@users.noreply.github.com>
2026-01-05 12:46:15 +0800

    Remove isHandicapped

--- src/parkingapp/Sedan.java
+++ src/parkingapp/Sedan.java
@@ -2,8 +2,8 @@ package parkingapp;
 
 public class Sedan extends Vehicle{
     
-    public Sedan(String platenumber, boolean isHandicapped, boolean isVip){
-        super(platenumber, isHandicapped, isVip, VehicleType.SEDAN);
+    public Sedan(String platenumber, boolean isVip){
+        super(platenumber, isVip, VehicleType.SEDAN);
     }
     
 }
