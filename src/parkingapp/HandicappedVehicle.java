89e9e36ed8d24630a578287dc8475948bb0a794d
CarrotIsTasty <138109195+CarrotIsTasty@users.noreply.github.com>
2026-01-05 12:49:49 +0800

    Create HandicappedVehicle.java

---
+++ src/parkingapp/HandicappedVehicle.java
@@ -0,0 +1,7 @@
+package parkingapp;
+
+public class HandicappedVehicle extends Vehicle{
+    public HandicappedVehicle(String platenumber, boolean isVip){
+        super(platenumber, isVip, VehicleType.HANDICAP);
+    }
+}
