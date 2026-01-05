64f64f5cd4b071a6d3ff8c61f19479c6f541ddf3
CarrotIsTasty <138109195+CarrotIsTasty@users.noreply.github.com>
2026-01-05 12:50:20 +0800

    Update HandicappedVehicle.java

--- src/parkingapp/HandicappedVehicle.java
+++ src/parkingapp/HandicappedVehicle.java
@@ -2,6 +2,6 @@ package parkingapp;
 
 public class HandicappedVehicle extends Vehicle{
     public HandicappedVehicle(String platenumber, boolean isVip){
-        super(platenumber, isVip, VehicleType.HANDICAP);
+        super(platenumber, isVip, VehicleType.HANDICAPPED);
     }
 }
